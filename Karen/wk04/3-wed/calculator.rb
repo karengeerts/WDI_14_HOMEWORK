dd6ecae815e7eb13f0323d87a0b7afe48f09f4a2
Karen Geerts <karen.geerts@gmail.com>
2017-12-20 20:05:30 +1100

    added while loop

--- Karen/wk04/3-wed/calculator.rb
+++ Karen/wk04/3-wed/calculator.rb
@@ -1,18 +1,29 @@
-puts "What operation do you want to perform? Press + or - or * or / "
-operation = gets.chomp
-p operation
+def calculator()
+  while true
+  puts "What operation do you want to perform? Press + or - or * or / or E (for exponents) or S (for square root)"
+  operation = gets.chomp
+  puts "What is the number you want to perform the operation on?"
+  num1 = gets.chomp.to_i
+  if operation !="S"
+  puts "What is the other number you want to perform the operation on?"
+  num2 = gets.chomp.to_i
+  end
 
-puts "What is the number you want to perform the operation on?"
-num1 = gets.chomp.to_i
-puts "What is the other number you want to perform the operation on?"
-num2 = gets.chomp.to_i
+  output = num1+num2 if operation == "+"
+  output = num1-num2 if operation == "-"
+  output = num1*num2 if operation == "*"
+  output = num1/num2 if operation == "/"
+  output = num1**num2 if operation == "E"
+  output = Math.sqrt(num1) if operation == "S"
 
-p num1
-p num2
+  puts "The result is #{output}"
 
-output = num1+num2 if operation == "+"
-output = num1-num2 if operation == "-"
-output = num1*num2 if operation == "*"
-output = num1/num2 if operation == "/"
+  puts "Do you want to perform another calculation? (y/n)?"
+  continue = gets.chomp
+  if continue != "y"
+    break
+  end
+end
+end
 
-puts "#{num1} #{operation} #{num2} equals #{output}"
+calculator()
