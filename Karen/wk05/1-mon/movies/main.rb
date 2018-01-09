5a1a8e405a47faf9f4fc7c83335b4cd9873d87e7
Karen Geerts <karen.geerts@gmail.com>
2018-01-09 20:37:34 +1100

    Tuesday homework sans extensions

--- Karen/wk05/1-mon/movies/main.rb
+++ Karen/wk05/1-mon/movies/main.rb
@@ -13,10 +13,23 @@ get '/about' do
   erb :about
 end
 
-
 get '/movie_input' do
-  title = params[:title]
-  movie_request = "http://www.omdbapi.com/?apikey=2f6435d9&t=" + title.to_s
+  movie_request = "http://www.omdbapi.com/?apikey=2f6435d9&s=#{params[:title]}"
+  @movies = HTTParty.get(movie_request)
+
+  counter = 0
+  @movie_results = []
+  while counter < @movies["Search"].length do
+    @name = @movies["Search"][counter]["Title"]
+    @imdbID = @movies["Search"][counter]["imdbID"]
+    counter += 1
+    @movie_results.push({name: @name, id: @imdbID})
+  end
+  erb :movies
+end
+
+get '/movie' do
+  movie_request = "http://www.omdbapi.com/?apikey=2f6435d9&i=#{params[:id]}"
   @result = HTTParty.get(movie_request)
   erb :movie
 end
