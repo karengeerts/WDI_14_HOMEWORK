5c1d0697f6eebca5d5f0ff20cd87b95377598dfc
Karen Geerts <karen.geerts@gmail.com>
2018-01-27 14:10:41 +1100

    Finished dropdown homework

---
+++ Karen/wk07/4-thu/drop_down/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('select[id=city-type]').onchange=changeEventHandler;
+},false);
+
+function changeEventHandler(event) {
+    if(!event.target.value) alert('Please Select One');
+    else if (event.target.value == "Austin") {
+        document.body.style.background = "url(../drop_down/images/austin.jpg) no-repeat center center fixed";
+    }
+    else if (event.target.value == "LA") {
+        document.body.style.background = "url(../drop_down/images/la.jpg) no-repeat center center fixed";
+    }
+    else if (event.target.value == "NYC") {
+        document.body.style.background = "url(../drop_down/images/nyc.jpg) no-repeat center center fixed";
+    }
+    else {
+      document.body.style.background = "url(../drop_down/images/sydney.jpg) no-repeat center center fixed";
+    }
+}
