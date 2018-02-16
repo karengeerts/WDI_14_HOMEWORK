efbfc483d3f11df59be3c6608cd0072ee775b767
Karen Geerts <karen.geerts@gmail.com>
2018-02-16 16:15:57 +1100

    stored API key in ENV

--- Karen/wk10/5-fri/app.js
+++ Karen/wk10/5-fri/app.js
@@ -1,6 +1,7 @@
+var open_weather_api_key = process.env.OPEN_WEATHER;
 var aussie_city = process.argv[2];
 
 var request = require('request');
-request('http://api.openweathermap.org/data/2.5/weather?q=' + aussie_city + ',Australia&units=metric&appid=c6697158c41d4842e9031e32615b0532', function (error, response, body) {
+request('http://api.openweathermap.org/data/2.5/weather?q=' + aussie_city + ',Australia&units=metric&appid=' + open_weather_api_key, function (error, response, body) {
   console.log('Temperature in ' + aussie_city + ':', JSON.parse(body).main.temp);
 });
