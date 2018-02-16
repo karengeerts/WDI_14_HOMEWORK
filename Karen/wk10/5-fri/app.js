4bb98afe1b0d507d17d70a4354f583eea17a0dd9
Karen Geerts <karen.geerts@gmail.com>
2018-02-16 15:41:26 +1100

    Friday hw with CLI input working

--- Karen/wk10/5-fri/app.js
+++ Karen/wk10/5-fri/app.js
@@ -1,4 +1,6 @@
+var aussie_city = process.argv[2];
+
 var request = require('request');
-request('http://api.openweathermap.org/data/2.5/weather?q=Melbourne,Australia&units=metric&appid=c6697158c41d4842e9031e32615b0532', function (error, response, body) {
-  console.log('Temperature in Melbourne:', JSON.parse(body).main.temp);
+request('http://api.openweathermap.org/data/2.5/weather?q=' + aussie_city + ',Australia&units=metric&appid=c6697158c41d4842e9031e32615b0532', function (error, response, body) {
+  console.log('Temperature in ' + aussie_city + ':', JSON.parse(body).main.temp);
 });
