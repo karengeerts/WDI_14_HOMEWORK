600ae1fdf08c4a8b851ebeeb5ffc81685c8fcd6b
Karen Geerts <karen.geerts@gmail.com>
2018-02-22 15:55:54 +1100

    password strength hw

---
+++ Karen/wk11/3-wed/password/app/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PwdMeter from './PwdMeter'
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    return <div>
+        <PwdMeter />
+    </div>
+  }
+
+}
